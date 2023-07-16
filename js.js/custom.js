737cf9f5d1a45dd7526834a06c995bc1ad285d33
John Doe <johndoe@example.com>
2023-07-17 01:36:59 +0400

    first commit

---
+++ js.js/custom.js
@@ -0,0 +1,16 @@
+/*global $,alert , console
+$(function() {
+    'ueser strict';
+    var heder = $('.heads');
+    heder.height($(window).height());
+    $(window).resize(function() {
+        heder.height($(window).height());
+    });
+    $('atrb li').click(function() {
+        $(this).addclass('active').siblings().removeclass('active')
+    });
+
+
+
+
+});*/
